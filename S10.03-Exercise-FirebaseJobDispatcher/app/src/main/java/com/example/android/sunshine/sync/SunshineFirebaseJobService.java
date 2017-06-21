0da83e88a1858fa6cfe27340b0353b1a8bb8c98c
Aidar Babanov <aidar.babanov@nu.edu.kz>
2017-06-21 17:24:57 +0600

    T10

--- S10.03-Exercise-FirebaseJobDispatcher/app/src/main/java/com/example/android/sunshine/sync/SunshineFirebaseJobService.java
+++ S10.03-Exercise-FirebaseJobDispatcher/app/src/main/java/com/example/android/sunshine/sync/SunshineFirebaseJobService.java
@@ -1,4 +1,4 @@
-/*
+package com.example.android.sunshine.sync;/*
  * Copyright (C) 2016 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,11 +15,46 @@
  */
 // TODO (2) Make sure you've imported the jobdispatcher.JobService, not job.JobService
 
-// TODO (3) Add a class called SunshineFirebaseJobService that extends jobdispatcher.JobService
+import android.content.Context;
+import android.os.AsyncTask;
 
-//  TODO (4) Declare an ASyncTask field called mFetchWeatherTask
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+
+// TODO (3) Add a class called SunshineFirebaseJobService that extends jobdispatcher.JobService
+class  SunshineFirebaseJobService extends JobService{
+
+    //  TODO (4) Declare an ASyncTask field called mFetchWeatherTask
 
 //  TODO (5) Override onStartJob and within it, spawn off a separate ASyncTask to sync weather data
 //              TODO (6) Once the weather data is sync'd, call jobFinished with the appropriate arguments
 
 //  TODO (7) Override onStopJob, cancel the ASyncTask if it's not null and return true
+
+    AsyncTask mFetchWeatherTask;
+
+    @Override
+    public boolean onStartJob(final JobParameters jobParameters) {
+        mFetchWeatherTask = new AsyncTask<Void, Void, Void >(){
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                Context context = getApplicationContext();
+                SunshineSyncTask.syncWeather(context);
+                jobFinished(jobParameters, false);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                jobFinished(jobParameters, false);
+            }
+        };
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters job) {
+        return false;
+    }
+}
